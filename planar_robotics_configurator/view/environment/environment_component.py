35d6f2f46003256ba49f7b4e4a912d6091d44e13
hheitkaemper <hheitkaemper@techfak.uni-bielefeld.de>
2024-06-19 14:36:32 +0200

    Moved environment selection class to own file.

--- planar_robotics_configurator/view/environment/environment_component.py
+++ planar_robotics_configurator/view/environment/environment_component.py
@@ -1,53 +1,10 @@
-from kivy.metrics import dp
-from kivymd.uix.anchorlayout import MDAnchorLayout
-from kivymd.uix.button import MDIconButton
 from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.gridlayout import MDGridLayout
-from kivymd.uix.menu import MDDropdownMenu
 
 from planar_robotics_configurator.model.configurator_model import ConfiguratorModel
 from planar_robotics_configurator.model.environment.environment import Environment
 from planar_robotics_configurator.view.environment.environment_map import EnvironmentMap
-from planar_robotics_configurator.view.utils import CustomLabel, AdaptiveDropDownItem, Component
-
-
-class EnvironmentSelection(MDAnchorLayout):
-    """
-    Defines an overlay for selecting the environment in the right top corner.
-    This layout provides a dropdown menu for selecting an environment with all environments saved in the model.
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.anchor_x = "right"
-        self.anchor_y = "top"
-        self.padding = [dp(0), dp(10), dp(10), dp(0)]
-        create_button = MDIconButton(icon="plus", theme_text_color="Custom", text_color=(1, 1, 1, 1))
-        create_button._default_icon_pad /= 4
-        self.dropdown_item = AdaptiveDropDownItem(adaptive_width=True)
-        self.dropdown_item.set_item(
-            ConfiguratorModel().environments[0].name if len(ConfiguratorModel().environments) > 0 else "None")
-        self.dropdown_item.on_release = self.open_menu
-        self.dropdown_menu = MDDropdownMenu(position='bottom', caller=self.dropdown_item)
-        self.add_widget(MDGridLayout(
-            CustomLabel(text="Current configuration", padding=[dp(4), dp(0), dp(8), dp(8)]),
-            MDGridLayout(self.dropdown_item, create_button, rows=1, adaptive_width=True),
-            cols=1, size_hint=(None, None), adaptive_width=True)
-        )
-
-    def open_menu(self):
-        self.dropdown_menu.items = []
-        for environment in ConfiguratorModel().environments:
-            self.dropdown_menu.items.append({
-                "text": environment.name,
-                "on_release": lambda x=environment: self.select_item(x)
-            })
-        self.dropdown_menu.open()
-
-    def select_item(self, environment: Environment):
-        self.dropdown_item.set_item(environment.name)
-        self.parent.set_environment(environment)
-        self.dropdown_menu.dismiss()
+from planar_robotics_configurator.view.environment.environment_selection import EnvironmentSelection
+from planar_robotics_configurator.view.utils import Component
 
 
 class EnvironmentComponent(MDFloatLayout, Component):
@@ -61,7 +18,7 @@ class EnvironmentComponent(MDFloatLayout, Component):
         self.environment: Environment | None = None
         self.map = EnvironmentMap()
         self.add_widget(self.map)
-        self.add_widget(EnvironmentSelection())
+        self.add_widget(EnvironmentSelection(self))
 
     def on_select(self, _):
         if self.environment is not None:
---
+++ planar_robotics_configurator/view/environment/environment_selection.py
@@ -0,0 +1,47 @@
+from kivy.metrics import dp
+from kivymd.uix.anchorlayout import MDAnchorLayout
+from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.menu import MDDropdownMenu
+
+from planar_robotics_configurator.model.configurator_model import ConfiguratorModel
+from planar_robotics_configurator.model.environment.environment import Environment
+from planar_robotics_configurator.view.utils import AdaptiveDropDownItem, CustomLabel, CustomIconButton
+
+
+class EnvironmentSelection(MDAnchorLayout):
+    """
+    Defines an overlay for selecting the environment in the right top corner.
+    This layout provides a dropdown menu for selecting an environment with all environments saved in the model.
+    """
+
+    def __init__(self, env_component, **kwargs):
+        super().__init__(**kwargs)
+        self.env_component = env_component
+        self.anchor_x = "right"
+        self.anchor_y = "top"
+        self.padding = [dp(0), dp(10), dp(10), dp(0)]
+        create_button = CustomIconButton(icon="plus")
+        self.dropdown_item = AdaptiveDropDownItem(adaptive_width=True)
+        self.dropdown_item.set_item(
+            ConfiguratorModel().environments[0].name if len(ConfiguratorModel().environments) > 0 else "None")
+        self.dropdown_item.on_release = self.open_menu
+        self.dropdown_menu = MDDropdownMenu(position='bottom', caller=self.dropdown_item)
+        self.add_widget(MDGridLayout(
+            CustomLabel(text="Current configuration", padding=[dp(4), dp(0), dp(8), dp(8)]),
+            MDGridLayout(self.dropdown_item, create_button, rows=1, adaptive_width=True),
+            cols=1, size_hint=(None, None), adaptive_width=True)
+        )
+
+    def open_menu(self):
+        self.dropdown_menu.items = []
+        for environment in ConfiguratorModel().environments:
+            self.dropdown_menu.items.append({
+                "text": environment.name,
+                "on_release": lambda x=environment: self.select_item(x)
+            })
+        self.dropdown_menu.open()
+
+    def select_item(self, environment: Environment):
+        self.dropdown_item.set_item(environment.name)
+        self.env_component.set_environment(environment)
+        self.dropdown_menu.dismiss()
