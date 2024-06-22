ca873e47a0010443222f7677963e286db0164131
hheitkaemper <hheitkaemper@techfak.uni-bielefeld.de>
2024-06-23 00:43:25 +0200

    Edit environment and added update_tiles.
    Added the height and mass params for environment. Added method to update the tiles without a reset of the current tiles configuration.

--- planar_robotics_configurator/model/environment/environment.py
+++ planar_robotics_configurator/model/environment/environment.py
@@ -12,17 +12,21 @@ class Environment:
     :param tiles: [num_width, num_length] Array of int. 1 for a tile at the position, 0 else
     :param tile_width: width (cm) of the tiles in the environment in x direction.
     :param tile_length: length (cm) of the tiles in the environment in y direction.
-    :param table_height: height of the table in the environment.
+    :param tile_height: height (cm) of the tiles in the environment in z direction.
+    :param tile_mass: mass (mg) of the tiles in the environment.
+    :param table_height: height (cm) of the table in the environment.
     :param std_noise: Standard deviation of the noise in the environment.
     """
     name: str
-    num_width: int = 10
-    num_length: int = 10
-    tiles: np.ndarray[int] = field(init=False)
-    tile_width: float = 25
-    tile_length: float = 25
-    table_height: float = 0.2
-    std_noise: float = 0.5
+    num_width: int
+    num_length: int
+    tiles: np.ndarray = field(init=False)
+    tile_width: float
+    tile_length: float
+    tile_height: float
+    tile_mass: float
+    table_height: float
+    std_noise: float
 
     def __post_init__(self):
         self.init_tiles()
@@ -33,6 +37,30 @@ class Environment:
         Sets all tiles to zero, which represents a not existing tile.
         """
         self.tiles = np.zeros((self.num_width, self.num_length), dtype=int)
+
+    def update_tiles(self):
+        """
+        Updates the tiles array. Expands or slices the current tiles to create an array matching the needed size.
+        """
+        if self.num_width != self.tiles.shape[0]:
+            if self.num_width < self.tiles.shape[0]:
+                self.tiles = self.tiles[:self.num_width, :]
+            else:
+                self.tiles = np.concatenate(
+                    (self.tiles, np.zeros((self.num_width - self.tiles.shape[0],
+                                           self.tiles.shape[1]), dtype=int)), axis=0)
+        if self.num_length != self.tiles.shape[1]:
+            if self.num_length < self.tiles.shape[1]:
+                self.tiles = self.tiles[:, :self.num_length]
+            else:
+                self.tiles = np.concatenate((
+                    self.tiles, np.zeros((self.tiles.shape[0],
+                                          self.num_length - self.tiles.shape[1]), dtype=int)), axis=1)
+
+    def set_size(self, num_width: int, num_length: int):
+        self.num_width = num_width
+        self.num_length = num_length
+        self.update_tiles()
 
     def get_tile(self, x, y) -> bool:
         """
