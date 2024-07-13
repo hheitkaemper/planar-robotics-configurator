1244c877dd1c1bac0f1ecb67658ad8a658e5e636
hheitkaemper <hheitkaemper@techfak.uni-bielefeld.de>
2024-07-13 14:04:23 +0200

    Fixed multiple top level packages.

--- planar_robotics_configurator/configurator.py
+++ planar_robotics_configurator/configurator.py
@@ -4,7 +4,7 @@ from planar_robotics_configurator.model.config import Config
 from planar_robotics_configurator.model.configurator_model import ConfiguratorModel
 
 
-@hydra.main(version_base=None, config_path='../.config', config_name='config')
+@hydra.main(version_base=None, config_path='../config', config_name='config')
 def main(config: Config) -> None:
     ConfiguratorModel().load_config(config)
     from planar_robotics_configurator.view.configurator_app import ConfiguratorApp
