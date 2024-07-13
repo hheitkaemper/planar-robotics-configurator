b250c03642b4d92d709ab696ab687227360a8cd4
hheitkaemper <hheitkaemper@techfak.uni-bielefeld.de>
2024-07-13 15:42:33 +0200

    Updated map hide settings.
    It is now possible to intend entries and entries can be added easier.

--- planar_robotics_configurator/view/environment/dialog/hide_settings.py
+++ planar_robotics_configurator/view/environment/dialog/hide_settings.py
@@ -11,44 +11,34 @@ class HideSettings(ScrollDialog):
 
     def __init__(self, env_map):
         self.env_map = env_map
+        self.hide_list = []
         super().__init__("Hide Settings", "Cancel", "Confirm")
-        self.show_env_background = env_map.hiding_settings["environment_background"]
-        self.add_option("Show Environment Background", self.show_env_background,
-                        lambda instance: self.__setattr__("show_env_background", instance.active))
-        self.show_tiles = env_map.hiding_settings["tiles"]
-        self.add_option("Show Tiles", self.show_tiles,
-                        lambda instance: self.__setattr__("show_tiles", instance.active))
-        self.show_movers = env_map.hiding_settings["movers"]
-        self.add_option("Show Movers", self.show_movers,
-                        lambda instance: self.__setattr__("show_movers", instance.active))
-        self.show_movers_collision = env_map.hiding_settings["movers_collision"]
-        self.add_option("Show Movers Collision", self.show_movers_collision,
-                        lambda instance: self.__setattr__("show_movers_collision", instance.active))
-        self.show_working_stations = env_map.hiding_settings["working_stations"]
-        self.add_option("Show Working Stations", self.show_working_stations,
-                        lambda instance: self.__setattr__("show_working_stations", instance.active))
-        self.show_objects = env_map.hiding_settings["objects"]
-        self.add_option("Show Objects", self.show_objects,
-                        lambda instance: self.__setattr__("show_objects", instance.active))
+        self.add_option("environment_background", "Show Environment Background")
+        self.add_option("tiles", "Show Tiles")
+        self.add_option("movers", "Show Movers")
+        self.add_option("movers_collision", "Show Movers Collision", indent=1)
+        self.add_option("working_stations", "Show Working Stations")
+        self.add_option("working_stations_name", "Show Working Stations Name", indent=1)
+        self.add_option("objects", "Show Objects")
+        self.add_option("objects_name", "Show Objects Name", indent=1)
 
     def on_confirm(self):
-        self.env_map.hiding_settings["environment_background"] = self.show_env_background
-        self.env_map.hiding_settings["tiles"] = self.show_tiles
-        self.env_map.hiding_settings["movers"] = self.show_movers
-        self.env_map.hiding_settings["movers_collision"] = self.show_movers_collision
-        self.env_map.hiding_settings["working_stations"] = self.show_working_stations
-        self.env_map.hiding_settings["objects"] = self.show_objects
+        for hide in self.hide_list:
+            self.env_map.hiding_settings[hide] = self.__dict__[hide]
         self.env_map.redraw()
         self.dismiss()
 
     def on_cancel(self):
         self.dismiss()
 
-    def add_option(self, text, active, callback):
+    def add_option(self, att, text, indent=0):
+        self.hide_list.append(att)
+        self.__dict__[att] = self.env_map.hiding_settings[att]
         layout = MDBoxLayout(orientation="horizontal", adaptive_height=True, spacing=dp(20))
+        layout.padding[0] = dp(40*indent)
         checkbox = CustomCheckbox(size_hint=(None, None), width=dp(20), height=dp(20),
-                                  pos_hint={"center_y": 0.5}, active=active)
-        checkbox.bind(on_release=callback)
+                                  pos_hint={"center_y": 0.5}, active=self.__dict__[att])
+        checkbox.bind(on_release=lambda instance: self.__setattr__(att, instance.active))
         layout.add_widget(checkbox)
         layout.add_widget(CustomLabel(text=text, pos_hint={"center_y": 0.5}))
         self.add_scroll_widget(layout)
